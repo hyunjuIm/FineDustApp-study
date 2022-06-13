d2fc8f4e9c66fd9034faba84799ad69f6929732f
hyunju <dear.jjwim@gmail.com>
2022-06-13 19:11:50 +0900

    위젯 레이아웃 구현

---
+++ app/src/main/java/com/example/finedustapp/appwidget/SimpleAirQualityWidgetProvider.kt
@@ -0,0 +1,15 @@
+package com.example.finedustapp.appwidget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class SimpleAirQualityWidgetProvider:AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+    }
+}
